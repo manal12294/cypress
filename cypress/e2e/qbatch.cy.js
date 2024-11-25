09dc2e14d1a2acceed42fdd5ab3a03a33f60455c
qbatch <qbatch@qbatchs-MacBook-Pro.local>
2024-11-25 13:24:36 +0500

    commit second

--- cypress/e2e/qbatch.cy.js
+++ cypress/e2e/qbatch.cy.js
@@ -3,19 +3,26 @@ describe('Navigation Links Test', () => {
     // Step 1: Visit the website
     cy.visit('https://qbatch.com'); 
 
-    // Step 2: Wait for and interact with the navigation menu (using XPath or CSS selector)
-    cy.xpath('/html/body/div[1]/div[1]/div[1]', { timeout: 10000 }).should('be.visible'); // Wait for the page container
+    // Step 2: Wait for and interact with the navigation menu (using XPath)
+    cy.xpath('/html/body/div[1]/div[1]/div[1]/div', { timeout: 10000 }).should('be.visible'); // Wait for the page container
 
     // Step 3: Click the 'Services' link inside the container div and check the URL
-    cy.xpath('/html/body/div[1]/div[1]/div[1]').contains('Services').click();
+    cy.xpath('/html/body/div[1]/div[1]/div[1]/div/div/div[2]/ul/li[1]/div[1]/a').contains('Services').click();
     cy.url().should('eq', 'https://qbatch.com/services/');
 
     // Step 4: Click the 'Hire Developers' link inside the container div and check the URL
-    cy.xpath('/html/body/div[1]/div[1]/div[1]').contains('Hire Developers').click();
+    cy.xpath('/html/body/div[1]/div[1]/div[1]/div').contains('Hire Developers').click();
     cy.url().should('eq', 'https://qbatch.com/hire-developers/');
 
-    // Step 4: Click the 'Hire Developers' link inside the container div and check the URL
-    cy.xpath('/html/body/div[1]/div[1]/div[1]').contains('Blog').click();
+    // Step 5: Click the 'Blog' link inside the container div and check the URL
+    cy.xpath('/html/body/div[1]/div[1]/div[1]/div').contains('Blog').click();
     cy.url().should('eq', 'https://qbatch.com/blog/');
+
+    
+   
   });
 });
+
+
+
+
